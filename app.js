06bfd91c016b2d123276377fcd2b3283bf06bf72
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2019-01-26 13:20:35 +0530

    First App with eslint configured , nodemon configured

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+
+const port = process.env.PORT || 4600;
+
+app.get('/', (err, res) => {
+    res.send('Welcom to the first app :)');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is up & running on port : ${port} !!!`);
+});
