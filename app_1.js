27e327e2e08fb1505d0204af98cf6a2fd128c73f
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2019-01-26 21:31:28 +0530

    GET implemented with mock data

--- app_1.js
+++ app_1.js
@@ -1,8 +1,20 @@
 const express = require('express');
 
+const wrestlerRouter = express.Router();
+
 const app = express();
 
 const port = process.env.PORT || 4600;
+
+wrestlerRouter.route('/wrestlers')
+  .get((req, res) => {
+    const response = {
+      hello: 'hello',
+    };
+    res.json(response);
+  });
+
+app.use('/api', wrestlerRouter);
 
 app.get('/', (re, res) => {
   res.send('Welcom to the first app :)');
