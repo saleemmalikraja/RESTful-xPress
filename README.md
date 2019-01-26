581715a434bf4213e4c96bb0eed66f40b853efb0
saleemmalikraja@gmail.com <saleemmalikraja@gmail.com>
2019-01-26 13:57:55 +0530

    Fixed lint errors

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RESTful-xPress
 RESTful webserices with Node JS and Express
+
+
+#Tips
+If you are using vscode i would recommend you to click the option at the bottom-right of the window and set it to LF from CRLF
+
+#How to run ?
+npm start
--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 
 const app = express();
 
-
 const port = process.env.PORT || 4600;
 
 app.get('/', (re, res) => {
-    res.send('Welcom to the first app :)');
+  res.send('Welcom to the first app :)');
 });
 
-
 app.listen(port, () => {
-    console.log(`Server is up & running on port : ${port} !!!`);
+  console.log(`Server is up & running on port : ${port} !!!`);
 });
